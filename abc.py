3aa8915697843103a189d4036dbb51f1e39bea0a
IvanVereshchagin <151856964+IvanVereshchagin@users.noreply.github.com>
2024-05-25 11:25:55 +0000

    abc.py

--- abc.py
+++ abc.py
@@ -86,7 +86,7 @@ if st.button('Показать вопросы'):
             st.image(f'image{i}.jpg')
     
     ph = st.empty()
-    N = 5*60
+    N = 7*60
     for secs in range(N,0,-1):
         mm, ss = secs//60, secs%60
         ph.metric("Таймер", f"{mm:02d}:{ss:02d}")
